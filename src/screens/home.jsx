339c798f76fb97c2a9a92d7f5c174a36bbab1b69
NomadesPrime <fernandesrichard312@gmail.com>
2022-11-09 16:27:05 -0300

    home

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,4 +1,4 @@
-import { Grid, TextField } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
 import { StyleSheet, Text, View } from "react-native";
 
 export const home = () => (
@@ -8,12 +8,27 @@ export const home = () => (
     <View style={styles.div}>
       <Grid style={styles.grid} container spacing={-1}>
         <Grid xs={6}>
-        <TextField style={styles.input} id="standard-basic" label="Nome" variant="filled" />
+          <TextField
+            style={styles.input}
+            id="standard-basic"
+            label="Nome"
+            variant="filled"
+          />
         </Grid>
         <Grid>
-        <TextField style={styles.input} id="outlined-basic" label="Sobrenome" variant="filled" />
+          <TextField
+            style={styles.input}
+            id="outlined-basic"
+            label="Sobrenome"
+            variant="filled"
+          />
         </Grid>
       </Grid>
+    </View>
+    <View style={styles.div}>
+      <Button style={styles.botao} variant="contained">
+        Enviar
+      </Button>
     </View>
   </View>
 );
@@ -34,17 +49,20 @@ const styles = StyleSheet.create({
     display: "flex",
     padding: 0,
     width: 150,
-
   },
   div: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    maxWidth:"768px",
+    margin: "auto",
   },
   grid: {
     alignItems: "center",
     margin: 50,
-    paddingLeft: 50,
-  
-  }
+    justifyContent: "center",
+  },
+  botao: {
+    width: "20rem",
+  },
 });
